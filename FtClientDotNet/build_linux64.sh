d8f2f263b0476717052dc22d346238f25151d891
Hyogeol Lee <hyogeollee@gmail.com>
2023-11-22 08:00:37 +0900

    Add build script

---
+++ FtClientDotNet/build_linux64.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm -rf _build_linux-x64
+
+dotnet publish --self-contained true -r linux-x64 -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -c Release -o _build_linux-x64 TestRead/TestRead.csproj
+
+dotnet publish --self-contained true -r linux-x64 -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -c Release -o _build_linux-x64 TestWrite/TestWrite.csproj
+
+cp ./ext/Linux/x64/libftd3xx.so ./_build_linux-x64
+
+rm -f ./_build_linux-x64/*.pdb
---
+++ FtClientDotNet/build_win64.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+rd /s /q _build_win-x64
+
+dotnet publish --self-contained true -r win-x64 -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -c Release -o _build_win-x64 TestRead\TestRead.csproj
+
+dotnet publish --self-contained true -r win-x64 -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -c Release -o _build_win-x64 TestWrite\TestWrite.csproj
+
+copy ext\Windows\x64\FTD3xx.dll _build_win-x64
+
+del _build_win-x64\*.pdb
